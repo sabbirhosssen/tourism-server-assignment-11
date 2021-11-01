a79fd4a4435e58cce96e7939b97c411665bd1a92
sabbir <sabbirhossen58sh58gmail.com>
2021-11-01 23:45:12 +0600

    server site diploy

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { MongoClient } = require('mongodb')
+const cors = require('cors')
 require('dotenv').config()
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 
-
+app.use(cors())
+app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zbsq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 
@@ -19,16 +21,12 @@ async function run() {
 
         //Post Api
         app.post('/tourShiping', async (req, res) => {
-            const tour = {
-                "img": "https://tour.com.bd/uploads/images/tours/slider/370472_M.V-Aboshar-2.jpg",
-                "name": "SUNDARBAN TOUR KHULNA-Dhaka",
-                "address": "Dhaka",
-                "price": " 12000",
-                "description": "Arrive at Khulna in the morning and transfer to our vessel...The boat will immediately start its journey towards the Sundarban forest. On the way, the boat will stop at Karamjal Eco Park and explore the mangrove forest walking through "
-            }
-            // const result = await toursCollection.insertOne(tour)
-            // console.log(result);
+            const tours = req.body;
+            console.log('hit the console api', tours);
 
+            const result = await toursCollection.insertOne(tours)
+            console.log(result);
+            res.json(result)
         })
 
     }
